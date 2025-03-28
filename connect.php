69e1fe41cc7cf405640cc5a39f995c87e16a556f
PaulTDV <tardivelpaul22@gmail.com>
2025-03-28 10:24:22 +0100

    test

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-	    define ('SERVEUR_BD','localhost');
-	    define ('LOGIN_BD','root');
-	    define ('PASS_BD','');
-	    define ('NOM_BD','test'); 
-        ?>
-         
+// Définition des constantes de connexion (remplace localhost par c_mariadb)
+define('SERVEUR_BD', 'c_mariadb'); // Nom du container Docker
+define('LOGIN_BD', 'MIAOU'); // Doit correspondre à MARIADB_USER
+define('PASS_BD', 'MEOW'); // Doit correspondre à MARIADB_PASSWORD
+define('NOM_BD', 'rocketleague'); // Doit correspondre à MARIADB_DATABASE
+?>
--- sql/init-base.sql
+++ sql/init-base.sql
@@ -1,7 +1,68 @@
-CREATE DATABASE IF NOT EXISTS rocket_league;
-USE rocket_league;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 28 mars 2025 à 09:14
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
-CREATE TABLE IF NOT EXISTS votes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    mode VARCHAR(20) NOT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `rocket_league`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `votes`
+--
+
+CREATE TABLE `votes` (
+  `id` int(11) NOT NULL,
+  `mode` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `votes`
+--
+
+INSERT INTO `votes` (`id`, `mode`) VALUES
+(1, '1v1'),
+(2, '3v3'),
+(3, 'Dropshot'),
+(4, 'Dropshot');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
